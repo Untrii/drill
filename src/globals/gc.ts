4e8b1732ba59f2fabb0bd2f86d6495807c2e2f27
Untrii <snick159ers@gmail.com>
2025-03-18 11:42:01 +0300

    Added min gc interval

--- src/globals/gc.ts
+++ src/globals/gc.ts
@@ -4,8 +4,14 @@ import { runInNewContext } from 'node:vm'
 setFlagsFromString('--expose_gc')
 const gc = runInNewContext('gc')
 
+const GC_TRESHOLD = 32 * 1024 * 1024
+const GC_INTERVAL = 5000
+
+let lastGcTimestamp = 0
+
 export function tryGC() {
-  if (process.memoryUsage().arrayBuffers > 32 * 1024 * 1024) {
+  if (process.memoryUsage().arrayBuffers > GC_TRESHOLD && Date.now() - lastGcTimestamp > GC_INTERVAL) {
+    lastGcTimestamp = Date.now()
     gc()
   }
 }
